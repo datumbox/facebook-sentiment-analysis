47045a94273cbe883dfba02922f0a2eae50c5b14
ecolosio <ecolosio@users.noreply.github.com>
2017-06-19 18:45:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,5 +4,6 @@
 define('01ad740ebbaedc74c22b58a988aefe35', '');
 
 // Configure your Facebook App and get your credentials from https://developers.facebook.com/
-define('01ad740ebbaedc74c22b58a988aefe35', '');
+define('1905104149755997', '');
 define('d57f737acaa523217155a665ca36f55b', ''); 
+
